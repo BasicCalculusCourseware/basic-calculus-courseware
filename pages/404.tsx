b0b1248d8a7e048cd124b910346a5216cf89fe2f
Arben Apura <arbenapura.official@gmail.com>
2022-06-03 05:04:50 +0800

    added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,24 @@
+// LIB FUNCTIONS
+import Link from 'next/link';
+// COMPONENTS
+import { Warning, WarningTextIcon, LinkTextSpan } from 'src/components/styled';
+import ViewSetter from 'src/components/setters/ViewSetter';
+
+// MAIN-COMPONENT
+export default function NotFound() {
+    return (
+        <ViewSetter pageBase="not-found" isPageUsingSidebar={false}>
+            <Warning>
+                <WarningTextIcon>404</WarningTextIcon>
+                <p>This page is currently unavailable or did not exist.</p>
+                <p>
+                    Go back to{' '}
+                    <Link href="/" passHref>
+                        <LinkTextSpan>home</LinkTextSpan>
+                    </Link>{' '}
+                    page?
+                </p>
+            </Warning>
+        </ViewSetter>
+    );
+}
