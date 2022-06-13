4ded814f1447f27adc424831fe553f4660cd5136
Arben Apura <arbenapura.official@gmail.com>
2022-06-13 15:13:16 +0800

    added fb admin dev

--- src/firebase/admin/index.ts
+++ src/firebase/admin/index.ts
@@ -2,10 +2,14 @@ import { getApps, initializeApp, cert, ServiceAccount } from 'firebase-admin/app
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import serviceAccountKey from './resources/service-account-key.json';
+import serviceAccountKeyDev from './resources/service-account-key-dev.json';
 
 if (!getApps().length) {
     initializeApp({
-        credential: cert(serviceAccountKey as ServiceAccount),
+        credential:
+            process.env.NEXT_PUBLIC_FB_ADMIN_TYPE === 'development'
+                ? cert(serviceAccountKeyDev as ServiceAccount)
+                : cert(serviceAccountKey as ServiceAccount),
         databaseURL: process.env.NEXT_PUBLIC_FB_DATABASE_URL,
     });
 }
